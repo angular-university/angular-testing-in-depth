5dbfc03e73668d7ab0aa9fa7b9138eaf5ea79bc4
vasco <jhades.dev@gmail.com>
2026-02-04 14:33:10 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/common/calculator.ts
+++ src/app/common/calculator.ts
@@ -1,5 +1,9 @@
 
-export function add(a: number, b: number) {
-  console.log('REAL add() implementation called');
+function add(a: number, b: number) {
+  console.log('REAL add() called');
   return a + b;
+}
+
+export const calculator = {
+  add
 }
