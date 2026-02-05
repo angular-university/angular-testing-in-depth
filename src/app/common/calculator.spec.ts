0c697fa2b6d7e0fd4abcfb23005768d4a648ee7b
vasco <jhades.dev@gmail.com>
2026-02-05 16:09:25 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/common/calculator.spec.ts
+++ src/app/common/calculator.spec.ts
@@ -4,9 +4,17 @@ import {calculator} from "./calculator";
 
 describe("Vitest Fundamentals", () => {
 
-  it.skip("should add two numbers", () =>{
+  it.skip("should add two numbers", () => {
     const result = calculator.add(2, 3);
     expect(result).toBe(5);
-  });
+  })
+
+  it("shows how spies work", () => {
+    const spy = vi.spyOn(calculator, "add");
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenCalledWith(2, 3);
+  })
 
 });
