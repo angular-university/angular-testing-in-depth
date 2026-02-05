8e4100704e9e0b6e09a99d078c6325266f12992c
vasco <jhades.dev@gmail.com>
2026-02-05 16:52:27 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/common/calculator.spec.ts
+++ src/app/common/calculator.spec.ts
@@ -4,7 +4,7 @@ import {calculator} from "./calculator";
 
 describe("Vitest Fundamentals", () => {
 
-  it.skip("should add two numbers", () => {
+  it("should add two numbers", () => {
     const result = calculator.add(2, 3);
     expect(result).toBe(5);
   })
@@ -15,6 +15,16 @@ describe("Vitest Fundamentals", () => {
     expect(result).toBe(5);
     expect(spy).toHaveBeenCalledOnce();
     expect(spy).toHaveBeenCalledWith(2, 3);
+  })
+
+  it.only("shows how mocking works", () => {
+    const spy = vi.spyOn(calculator, "add").mockReturnValue(5);
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenCalledWith(2, 3);
+    const result2 = calculator.add(5, 5);
+    expect(result2).toBe(10);
   })
 
 });
